14311d441e62882dbc0d1c88d9f937357e584e3f
varkenvarken <andersmichel8@gmail.com>
2024-11-09 11:08:27 +0000

    some tests

--- simulator/gate.py
+++ simulator/gate.py
@@ -1,7 +1,10 @@
 from typing import Sequence
-from abc import abstractmethod
+from abc import abstractmethod, ABCMeta
 
-class Producer:
+class ConfigurationException(Exception):
+    ...
+
+class Producer(metaclass=ABCMeta):
     ...
 
 class Gate(Producer):
@@ -10,4 +13,30 @@ class Gate(Producer):
 
     @abstractmethod
     def output(self) -> bool:
-        ...
+        ... # pragma: no cover
+
+    def sample_input(self, index:int) -> bool | None:
+        if index >= len(self.inputs) or self.inputs[index] is None:
+            return None
+        return self.inputs[index].output()
+
+class Value(Producer):
+    def __init__(self, value=False):
+        self.inputs = [value]
+    
+    def output(self) -> bool:
+        return self.inputs[0]
+    
+    def set(self, value:bool):
+        if type(value) is not bool:
+            raise TypeError("values can be booleans only")
+        self.inputs[0] = value
+
+class Connector(Gate):
+    def __init__(self, inputs:Sequence[Producer]):
+        if len(inputs) > 1:
+            raise ConfigurationException("Connector can have at most 1 input")
+        super().__init__(inputs)
+
+    def output(self) -> bool:
+        return self.sample_input(0)
--- test/test_gate.py
+++ test/test_gate.py
@@ -1,7 +1,47 @@
+"""
+Tests for simulator.gate
+"""
+
+import pytest
+
 from simulator import gate    # The code to test
 
+
 def test_gate():
-    g = gate.Gate([])
-    assert g is not None
+    """gate is an abstract base class and we should not be able to instantiate it."""
+    with pytest.raises(TypeError):
+        g = gate.Gate([])
 
 
+def test_value():
+    v = gate.Value()
+    assert v is not None
+    assert v.output() is False
+    v.set(True)
+    assert v.output() is True
+    v.set(False)
+    assert v.output() is False
+    with pytest.raises(TypeError):
+        v.set(0.0)
+
+
+def test_unconnected_connector():
+    c = gate.Connector([])
+    assert c is not None
+    assert c.output() is None
+
+
+def test_overconnected_connector():
+    with pytest.raises(gate.ConfigurationException):
+        c = gate.Connector([gate.Value(), gate.Value()])
+
+
+def test_connector_init_nosequence():
+    with pytest.raises(TypeError):
+        c = gate.Connector(gate.Value())
+
+
+def test_connector():
+    c = gate.Connector([gate.Value()])
+    assert c is not None
+    assert c.output() is False
