33046fbf7ffd19e77a63d5b0306b17fb520d4be2
varkenvarken <andersmichel8@gmail.com>
2024-11-10 13:37:49 +0000

    image based version of drawable

--- simulator/drawable.py
+++ simulator/drawable.py
@@ -1,74 +1,69 @@
 from abc import abstractmethod, ABCMeta
-from typing import Tuple
-from pygame import Surface, draw, Rect
+from typing import Tuple, Sequence
+from pathlib import Path
+
+import pygame
+from pygame import Surface, draw, Rect, transform, Vector2, image, time
 
 
 class Drawable:
-    def __init__(self, color, linewidth):
-        self.color = color
-        self.linewidth = linewidth
-
-    @property
-    def x(self):
-        return self.drawable.x
-
-    @x.setter
-    def x(self, value):
-        self.drawable.x = value
-
-    @property
-    def y(self):
-        return self.drawable.y
-
-    @y.setter
-    def y(self, value):
-        self.drawable.y = value
-
-    @property
-    def w(self):
-        return self.drawable.w
-
-    @w.setter
-    def w(self, value):
-        self.drawable.w = value
-
-    @property
-    def h(self):
-        return self.drawable.h
-
-    @h.setter
-    def h(self, value):
-        self.drawable.h = value
-
-    def collidepoint(self, *args, **kwargs):
-        return self.drawable.collidepoint(*args, **kwargs)
-
-    def __str__(self):
-        return f"{self.drawable}"
+    def __init__(self, pos):
+         self.selected = False
+         self.active = False
+         self.pos = Vector2(pos)
     
-class Rectangle(Drawable):
-    def __init__(self, x, y, w, h, color="black", linewidth=2):
-        self.drawable = pygame.Rect(x, y, w, h)
-        super().__init__(color, linewidth)
+    def blit(self, surface):
+        rect = self.surface.get_rect()
+        rect.center = self.pos
+        surface.blit(self.surface,rect)
+        if self.active:
+             draw.rect(surface, "orange", rect, 2)
+        if self.selected:
+             draw.rect(surface, "red", rect, 1)
+        
+    def rotate(self, angle):
+         self.surface = transform.rotate(self.surface, angle)
+    
+    def collidepoint(self, pos):
+        rect = self.surface.get_rect()
+        rect.center = self.pos
+        return rect.collidepoint(*pos)
+   
+class Image(Drawable):
+    def __init__(self, path, pos, size=None):
+        super().__init__(pos)
+        self.surface = image.load(Path(__file__).parent / path)
+        if size is not None:
+            self.surface = transform.smoothscale(self.surface, size)
+        self.surface = self.surface.convert_alpha()
 
-    def draw(self, screen:Surface):
-        pygame.draw.rect(screen, self.color, self.drawable, self.linewidth)
-
+class Circle(Drawable):
+     def __init__(self, pos, radius, color="black", linewidth=1):
+          super().__init__(pos)
+          self.surface = Surface((2*radius,2*radius), pygame.SRCALPHA)
+          draw.circle(self.surface, color, (radius,radius), radius, linewidth)
+          self.surface = self.surface.convert_alpha()
+  
 if __name__ == "__main__":
-    import pygame
+    FPS = 60
+
     pygame.init()
+
+    clock = time.Clock()
 
     width, height = 800, 600
     screen = pygame.display.set_mode((width, height))
     pygame.display.set_caption("Drawable test")
     screen.fill("white")
 
-    rectangles = [ Rectangle(width//2, height//2, 20, 10), Rectangle(width//4, height//4, 20, 10)]
+    drawables = [Image("icons/AND_ANSI_Labelled.svg", (x, 300)) for x in (200,400,600)]
 
     # Dragging control variables
     dragging = False
     dragged_rect_index = None
-    offset_x, offset_y = 0, 0
+    offset = Vector2(0,0)
+
+    active_object = None
 
     running = True
     while running:
@@ -77,31 +72,36 @@ if __name__ == "__main__":
             if event.type == pygame.QUIT:
                 running = False
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                for i,r in enumerate(rectangles):
+                for i, r in enumerate(drawables):
                     if r.collidepoint(event.pos):
+                        active_object = i
                         dragging = True
-                        dragged_rect_index = i  # later we work with more than one rectangle
+                        dragged_rect_index = i
                         # Calculate offset to maintain position relative to click
-                        offset_x = r.x - event.pos[0]
-                        offset_y = r.y - event.pos[1]
-                        break
+                        offset = r.pos - event.pos
+                        for d2 in drawables:
+                            d2.active = False
+                        r.active = True
             elif event.type == pygame.MOUSEBUTTONUP:
                 # Stop dragging when the mouse button is released
                 dragging = False
                 dragged_rect_index = None
             elif event.type == pygame.MOUSEMOTION:
-                # Update the position of the rectangle while dragging
+                # Update the position of the drawable while dragging
                 if dragging and dragged_rect_index is not None:
-                    r = rectangles[dragged_rect_index]
-                    r.x = event.pos[0] + offset_x
-                    r.y = event.pos[1] + offset_y
+                    r = drawables[dragged_rect_index]
+                    r.pos = event.pos + offset
+            elif event.type == pygame.KEYUP:
+                print(event)
+                if event.unicode == 'r' and active_object is not None:
+                    drawables[active_object].rotate(90)
 
         screen.fill("white")
-        
-        for r in rectangles:
-            r.draw(screen)
+
+        for d in drawables:
+            d.blit(screen)
         
         pygame.display.flip()
-        pygame.time.delay(30)
+        clock.tick(FPS)
 
     pygame.quit()
