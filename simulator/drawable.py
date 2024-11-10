b429f9a3b511c961c34e264cd7bf657989d8443f
varkenvarken <andersmichel8@gmail.com>
2024-11-10 14:23:07 +0000

    added overlay

--- simulator/drawable.py
+++ simulator/drawable.py
@@ -8,27 +8,28 @@ from pygame import Surface, draw, Rect, transform, Vector2, image, time
 
 class Drawable:
     def __init__(self, pos):
-         self.selected = False
-         self.active = False
-         self.pos = Vector2(pos)
-    
+        self.selected = False
+        self.active = False
+        self.pos = Vector2(pos)
+
     def blit(self, surface):
         rect = self.surface.get_rect()
         rect.center = self.pos
-        surface.blit(self.surface,rect)
+        surface.blit(self.surface, rect)
         if self.active:
-             draw.rect(surface, "orange", rect, 2)
+            draw.rect(surface, "orange", rect, 2)
         if self.selected:
-             draw.rect(surface, "red", rect, 1)
-        
+            draw.rect(surface, "red", rect, 1)
+
     def rotate(self, angle):
-         self.surface = transform.rotate(self.surface, angle)
-    
+        self.surface = transform.rotate(self.surface, angle)
+
     def collidepoint(self, pos):
         rect = self.surface.get_rect()
         rect.center = self.pos
         return rect.collidepoint(*pos)
-   
+
+
 class Image(Drawable):
     def __init__(self, path, pos, size=None):
         super().__init__(pos)
@@ -37,17 +38,40 @@ class Image(Drawable):
             self.surface = transform.smoothscale(self.surface, size)
         self.surface = self.surface.convert_alpha()
 
+
 class Circle(Drawable):
-     def __init__(self, pos, radius, color="black", linewidth=1):
-          super().__init__(pos)
-          self.surface = Surface((2*radius,2*radius), pygame.SRCALPHA)
-          draw.circle(self.surface, color, (radius,radius), radius, linewidth)
-          self.surface = self.surface.convert_alpha()
-  
+    def __init__(self, pos, radius, color="black", linewidth=1):
+        super().__init__(pos)
+        self.surface = Surface((2*radius, 2*radius), pygame.SRCALPHA)
+        draw.circle(self.surface, color, (radius, radius), radius, linewidth)
+        self.surface = self.surface.convert_alpha()
+
+
+class AndGate(Image):
+    def __init__(self, pos, size=None):
+        super().__init__("icons/AND_ANSI_Labelled.svg", pos, size)
+        r = self.surface.get_rect()
+        self.overlay = Surface(r.size, pygame.SRCALPHA).convert_alpha()
+        draw.circle(self.overlay, "black", (r.w-10,r.h//2),6,2)
+        draw.circle(self.overlay, "black", (10,r.h//3),6,2)
+        draw.circle(self.overlay, "black", (10,2*r.h//3),6,2)
+    
+    def blit(self, surface):
+        super().blit(surface)
+        rect = self.overlay.get_rect()
+        rect.center = self.pos
+        surface.blit(self.overlay, rect)
+
+    def rotate(self, angle):
+        super().rotate(angle)
+        self.overlay = transform.rotate(self.overlay, angle)
+
+
 if __name__ == "__main__":
     FPS = 60
 
     pygame.init()
+    pygame.display.set_icon(image.load(Path(__file__).parent / "icons/AND_ANSI_Labelled.svg"))
 
     clock = time.Clock()
 
@@ -56,12 +80,12 @@ if __name__ == "__main__":
     pygame.display.set_caption("Drawable test")
     screen.fill("white")
 
-    drawables = [Image("icons/AND_ANSI_Labelled.svg", (x, 300)) for x in (200,400,600)]
+    drawables = [AndGate((x, 300)) for x in (200, 400, 600)]
 
     # Dragging control variables
     dragging = False
     dragged_rect_index = None
-    offset = Vector2(0,0)
+    offset = Vector2(0, 0)
 
     active_object = None
 
@@ -100,7 +124,7 @@ if __name__ == "__main__":
 
         for d in drawables:
             d.blit(screen)
-        
+
         pygame.display.flip()
         clock.tick(FPS)
 
